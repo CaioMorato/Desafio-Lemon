e0ce639b629c54b92b26a68be1650439d562471e
Caio Morato <morato_dev@protonmail.com>
2022-03-21 23:54:05 -0300

    feat: adds cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // vitals
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 // setup
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
