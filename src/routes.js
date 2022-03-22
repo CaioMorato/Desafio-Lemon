65897dce585dfc4faf008eb3eb30a49b502f4fa6
Caio Morato <morato_dev@protonmail.com>
2022-03-21 22:25:56 -0300

    fix: removing test get route

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,6 @@
 const routes = require('express').Router();
 const { validateFields } = require('./middlewares/validations');
 
-routes.get('/', (req, res) => {
-  console.log('pong');
-  res.json('teste');
-});
 routes.post('/customer', validateFields);
 
 module.exports = routes;
