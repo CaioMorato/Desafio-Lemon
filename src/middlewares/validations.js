1fe6a7a423df7b367e2fa0a4a3e8770417671650
Caio Morato <morato_dev@protonmail.com>
2022-03-22 14:14:29 -0300

    fix: adding rule exception on eslint file to use return for the middlewares and controllers

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -5,12 +5,12 @@ const validateFields = async (req, res, next) => {
   try {
     const validate = await customerSchemaValidation.validate(req.body);
     if (validate.error) {
-      res.status(StatusCodes.BAD_REQUEST).json({ error: validate.error.details[0].message });
+      return res.status(StatusCodes.BAD_REQUEST).json({ error: validate.error.details[0].message });
     }
 
     next();
   } catch (err) {
-    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err });
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err });
   }
 };
 
