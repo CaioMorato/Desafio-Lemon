872fa05f1fc683f72ded3ccc2950c5c88cc719a3
Caio Morato <morato_dev@protonmail.com>
2022-03-21 22:25:04 -0300

    feat: implementing full route to POST JSON body

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -1,13 +1,16 @@
 const { StatusCodes } = require('http-status-codes');
 const { customerSchemaValidation } = require('../schemas/customerSchema');
 
-const validateFields = async (req, res) => {
+const validateFields = async (req, res, next) => {
   try {
     const validate = await customerSchemaValidation.validate(req.body);
-    console.log(validate);
+    if (validate.error) {
+      res.status(StatusCodes.BAD_REQUEST).json({ error: validate.error.details[0].message });
+    }
+
+    next();
   } catch (err) {
-    console.log(err);
-    res.status(StatusCodes.BAD_REQUEST).json({ error: err });
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: err });
   }
 };
 module.exports = { validateFields };
