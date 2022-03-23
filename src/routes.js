77892819ed2bf4c60d7b5ecfc620d4b9f70ea945
Caio Morato <morato_dev@protonmail.com>
2022-03-23 14:36:38 -0300

    feat: new route for get method, to test api in browser

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,8 @@ const { checkEligibility } = require('./controller/customerController');
 
 routes.post('/customer', validateFields, checkEligibility);
 
+routes.post('/', (req, res) => {
+  res.status(400).send({ status: 200, message: 'API funcionando!' });
+});
+
 module.exports = routes;
