6d4dbdad58a41e08bd20a59089527ce640c8513c
Caio Morato <morato_dev@protonmail.com>
2022-03-23 14:43:41 -0300

    fix: fixes the status for get requisition

--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,7 @@ const { checkEligibility } = require('./controller/customerController');
 routes.post('/customer', validateFields, checkEligibility);
 
 routes.post('/', (req, res) => {
-  res.status(400).send({ status: 200, message: 'API funcionando!' });
+  res.status(200).send({ status: 200, message: 'API funcionando!' });
 });
 
 module.exports = routes;
